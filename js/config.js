4d743dcc3ea0a9d3bc15adf017000a623bb2c2c5
Yifan Xiao <hempflower@outlook.com>
2019-07-25 10:06:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776858319-7c829d2c3e250f85776f588e', //Community
-	'm776858325-1ddf7c7e98ce56609325db3b', //Document
-	'm782409254-3c4b19cd3f7082f404df494e', //Me
-	'm776858407-a704cd548fb5c15db5b625f9', //Update A
-	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm778734196-a51947f061c5db10cee048f7', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+	'm783156246-f8985fc8bf0bb2de33b5a6de', //town
+	'm783154488-167643332f62e1b20d63010c', //mcskin
+	'm783154492-7066f149fcded692d7b51316', //mcskin(old)
+	'm783154455-e8548c9e2dfa5fa01e4fbfcc' //ORECRAFT
 ];
 
 // refresh interval (in seconds)
